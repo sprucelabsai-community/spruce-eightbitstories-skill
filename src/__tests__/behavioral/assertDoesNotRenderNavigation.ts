88afb85e483758b0a1ac7041c80cef2bfec57c29
Taylor Romero <me@taylorrome.ro>
2023-10-01 12:06:40 -0700

    patch: syntax fix, use this for supplimental 3, hiding buttons on story

--- src/__tests__/behavioral/assertDoesNotRenderNavigation.ts
+++ src/__tests__/behavioral/assertDoesNotRenderNavigation.ts
@@ -1,5 +1,7 @@
-import { assert } from '@sprucelabs/test-utils';
+import { assert } from '@sprucelabs/test-utils'
 
-export function assertDoesNotRenderNavigation(vc: { renderNavigation: () => null; }) {
-	assert.isNull(vc.renderNavigation());
+export function assertDoesNotRenderNavigation(vc: {
+	renderNavigation: () => null
+}) {
+	assert.isNull(vc.renderNavigation())
 }
