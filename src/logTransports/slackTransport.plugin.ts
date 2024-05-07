5de7350cfa129f9329335685650df30d0e999df9
Taylor Romero <me@taylorrome.ro>
2024-05-07 11:37:57 -0600

    patch: lint slack transport

--- src/logTransports/slackTransport.plugin.ts
+++ src/logTransports/slackTransport.plugin.ts
@@ -4,36 +4,38 @@ import { Level, LogTransport } from '@sprucelabs/spruce-skill-utils'
 import axios from 'axios'
 
 export const buildSlackTransport: (url: string, level: Level) => LogTransport =
-	function (url: string, level: Level) {
-		if (!url) {
-			throw new SchemaError({
-				code: 'MISSING_PARAMETERS',
-				parameters: ['url'],
-			})
-		}
+    function (url: string, level: Level) {
+        if (!url) {
+            throw new SchemaError({
+                code: 'MISSING_PARAMETERS',
+                parameters: ['url'],
+            })
+        }
 
-		if (!level) {
-			throw new SchemaError({
-				code: 'MISSING_PARAMETERS',
-				parameters: ['level'],
-			})
-		}
+        if (!level) {
+            throw new SchemaError({
+                code: 'MISSING_PARAMETERS',
+                parameters: ['level'],
+            })
+        }
 
-		return async (...messageParts: []) => {
-			const message =
-				`${os.hostname()} ${level} :: ` + messageParts.join(' ')
+        return async (...messageParts: []) => {
+            const message =
+                `${os.hostname()} ${level} :: ` + messageParts.join(' ')
 
-			try {
-				await axios({
-					url,
-					method: 'POST',
-					data: { text: message },
-					timeout: 1000,
-				})
-			} catch (err: any) {
-				console.error(
-					`Slack transport error reaching ${url}:\n\n` + err.stack + `\n\nOriginal Error: ${message}`
-				)
-			}
-		}
-	}
+            try {
+                await axios({
+                    url,
+                    method: 'POST',
+                    data: { text: message },
+                    timeout: 1000,
+                })
+            } catch (err: any) {
+                console.error(
+                    `Slack transport error reaching ${url}:\n\n` +
+                        err.stack +
+                        `\n\nOriginal Error: ${message}`
+                )
+            }
+        }
+    }
