d7282c40ffb5b273708da3a3953a35ff2546fefb
Taylor Romero <me@taylorrome.ro>
2024-09-10 14:02:40 -0600

    patch: event sync

---
+++ src/.spruce/schemas/eightbitstories/v2023_09_05/saveMeta.schema.ts
@@ -0,0 +1,31 @@
+import { SchemaRegistry } from '@sprucelabs/schema'
+import { SpruceSchemas } from '../../schemas.types'
+
+
+
+const saveMetaSchema: SpruceSchemas.Eightbitstories.v2023_09_05.SaveMetaSchema  = {
+	id: 'saveMeta',
+	version: 'v2023_09_05',
+	namespace: 'Eightbitstories',
+	name: '',
+	    fields: {
+	            /** Family Name. */
+	            'name': {
+	                label: 'Family Name',
+	                type: 'text',
+	                isRequired: true,
+	                options: undefined
+	            },
+	            /** Your Values. */
+	            'values': {
+	                label: 'Your Values',
+	                type: 'text',
+	                isRequired: true,
+	                options: undefined
+	            },
+	    }
+}
+
+SchemaRegistry.getInstance().trackSchema(saveMetaSchema)
+
+export default saveMetaSchema
