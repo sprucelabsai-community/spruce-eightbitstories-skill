9032c9c6c310a0fbcfa71478242b07be480bdb9d
Taylor Romero <me@taylorrome.ro>
2024-10-08 10:26:32 -0600

    patch: version updates

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -30,27 +30,6 @@ const listeners: EventFeatureListener[] = [
         isGlobal: require('../../meta/listeners/eightbitstories/save-meta.v2023_09_05.listener').isGlobal,
     },
     {
-        eventName: 'get-mmp-setup',
-        eventNamespace: 'eightbitstories',
-        version: 'v2023_09_05',
-        callback: require('../../mmp/listeners/eightbitstories/get-mmp-setup.v2023_09_05.listener').default,
-        isGlobal: require('../../mmp/listeners/eightbitstories/get-mmp-setup.v2023_09_05.listener').isGlobal,
-    },
-    {
-        eventName: 'generate-story',
-        eventNamespace: 'eightbitstories',
-        version: 'v2023_09_05',
-        callback: require('../../story/listeners/eightbitstories/generate-story.v2023_09_05.listener').default,
-        isGlobal: require('../../story/listeners/eightbitstories/generate-story.v2023_09_05.listener').isGlobal,
-    },
-    {
-        eventName: 'get-story',
-        eventNamespace: 'eightbitstories',
-        version: 'v2023_09_05',
-        callback: require('../../story/listeners/eightbitstories/get-story.v2023_09_05.listener').default,
-        isGlobal: require('../../story/listeners/eightbitstories/get-story.v2023_09_05.listener').isGlobal,
-    },
-    {
         eventName: 'add-family-member',
         eventNamespace: 'eightbitstories',
         version: 'v2023_09_05',
@@ -77,6 +56,27 @@ const listeners: EventFeatureListener[] = [
         version: 'v2023_09_05',
         callback: require('../../members/listeners/eightbitstories/update-family-member.v2023_09_05.listener').default,
         isGlobal: require('../../members/listeners/eightbitstories/update-family-member.v2023_09_05.listener').isGlobal,
+    },
+    {
+        eventName: 'get-mmp-setup',
+        eventNamespace: 'eightbitstories',
+        version: 'v2023_09_05',
+        callback: require('../../mmp/listeners/eightbitstories/get-mmp-setup.v2023_09_05.listener').default,
+        isGlobal: require('../../mmp/listeners/eightbitstories/get-mmp-setup.v2023_09_05.listener').isGlobal,
+    },
+    {
+        eventName: 'generate-story',
+        eventNamespace: 'eightbitstories',
+        version: 'v2023_09_05',
+        callback: require('../../story/listeners/eightbitstories/generate-story.v2023_09_05.listener').default,
+        isGlobal: require('../../story/listeners/eightbitstories/generate-story.v2023_09_05.listener').isGlobal,
+    },
+    {
+        eventName: 'get-story',
+        eventNamespace: 'eightbitstories',
+        version: 'v2023_09_05',
+        callback: require('../../story/listeners/eightbitstories/get-story.v2023_09_05.listener').default,
+        isGlobal: require('../../story/listeners/eightbitstories/get-story.v2023_09_05.listener').isGlobal,
     },
 ]
 
