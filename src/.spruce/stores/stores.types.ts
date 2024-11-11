7371058ab059622e28f8dcb77a08fec106433c68
Taylor Romero <me@taylorrome.ro>
2024-11-11 13:06:11 -0700

    patch: version updates

--- src/.spruce/stores/stores.types.ts
+++ src/.spruce/stores/stores.types.ts
@@ -1,17 +1,17 @@
 import FamilyMembersStore from '../../members/FamilyMembers.store'
-import StoriesStore from '../../story/Stories.store'
 import MetaStore from '../../meta/Meta.store'
+import StoriesStore from '../../story/Stories.store'
 
 declare module '@sprucelabs/data-stores/build/types/stores.types' {
 	interface StoreMap {
                 familyMembers: FamilyMembersStore
-                stories: StoriesStore
                 meta: MetaStore
+                stories: StoriesStore
 	}
 
 	interface StoreOptionsMap {
                 familyMembers: Omit<Parameters<typeof FamilyMembersStore['Store']>[0], keyof UniversalStoreOptions>   
-                stories: Omit<Parameters<typeof StoriesStore['Store']>[0], keyof UniversalStoreOptions>   
                 meta: Omit<Parameters<typeof MetaStore['Store']>[0], keyof UniversalStoreOptions>   
+                stories: Omit<Parameters<typeof StoriesStore['Store']>[0], keyof UniversalStoreOptions>   
         }
 }
--- src/.spruce/views/views.ts
+++ src/.spruce/views/views.ts
@@ -1,9 +1,9 @@
+import GenerateSkillViewController from '../../generation/Generate.svc'
 import MembersSkillViewController from '../../members/Members.svc'
 import MetaSkillViewController from '../../meta/Meta.svc'
-import GenerateSkillViewController from '../../generation/Generate.svc'
 import OnboardingSkillViewController from '../../onboarding/Onboarding.svc'
-import StorySkillViewController from '../../story/Story.svc'
 import RootSkillViewController from '../../skillViewControllers/Root.svc'
+import StorySkillViewController from '../../story/Story.svc'
 import FeedbackCardViewController from '../../feedback/FeedbackCard.vc'
 import FamilyMemberFormCardViewController from '../../members/FamilyMemberFormCard.vc'
 import AdjustMmpVcPlugin from '../../viewPlugins/mmp.view.plugin'
@@ -11,12 +11,12 @@ import AdjustMmpVcPlugin from '../../viewPlugins/mmp.view.plugin'
 import '@sprucelabs/heartwood-view-controllers'
 
 const vcs = {
+    GenerateSkillViewController,
     MembersSkillViewController,
     MetaSkillViewController,
-    GenerateSkillViewController,
     OnboardingSkillViewController,
-    StorySkillViewController,
     RootSkillViewController,
+    StorySkillViewController,
     FeedbackCardViewController,
     FamilyMemberFormCardViewController,
 }
@@ -29,32 +29,32 @@ type LoadOptions<Args extends Record<string,any>[]> = Args[0]['args'] extends Re
 
 declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.types' {
 	interface SkillViewControllerMap {
+		'eightbitstories.generate': GenerateSkillViewController
 		'eightbitstories.members': MembersSkillViewController
 		'eightbitstories.meta': MetaSkillViewController
-		'eightbitstories.generate': GenerateSkillViewController
 		'eightbitstories.onboarding': OnboardingSkillViewController
-		'eightbitstories.story': StorySkillViewController
 		'eightbitstories.root': RootSkillViewController
+		'eightbitstories.story': StorySkillViewController
 	}
 
 	interface SkillViewControllerArgsMap {
+		'eightbitstories.generate': LoadOptions<Parameters<GenerateSkillViewController['load']>>
 		'eightbitstories.members': LoadOptions<Parameters<MembersSkillViewController['load']>>
 		'eightbitstories.meta': LoadOptions<Parameters<MetaSkillViewController['load']>>
-		'eightbitstories.generate': LoadOptions<Parameters<GenerateSkillViewController['load']>>
 		'eightbitstories.onboarding': LoadOptions<Parameters<OnboardingSkillViewController['load']>>
-		'eightbitstories.story': LoadOptions<Parameters<StorySkillViewController['load']>>
 		'eightbitstories.root': LoadOptions<Parameters<RootSkillViewController['load']>>
+		'eightbitstories.story': LoadOptions<Parameters<StorySkillViewController['load']>>
 	}
 
 	interface ViewControllerMap {
 		'eightbitstories.feedback-card': FeedbackCardViewController
 		'eightbitstories.family-member-form-card': FamilyMemberFormCardViewController
+		'eightbitstories.generate': GenerateSkillViewController
 		'eightbitstories.members': MembersSkillViewController
 		'eightbitstories.meta': MetaSkillViewController
-		'eightbitstories.generate': GenerateSkillViewController
 		'eightbitstories.onboarding': OnboardingSkillViewController
-		'eightbitstories.story': StorySkillViewController
 		'eightbitstories.root': RootSkillViewController
+		'eightbitstories.story': StorySkillViewController
 	}
 
     interface ViewControllerOptionsMap {
@@ -70,7 +70,7 @@ declare module '@sprucelabs/heartwood-view-controllers/build/types/heartwood.typ
 //@ts-ignore
 if(typeof heartwood === 'function') { 
 	//@ts-ignore
-	heartwood(vcs, pluginsByName) 
+	heartwood({ vcs, pluginsByName }) 
 }
 
 export default vcs
